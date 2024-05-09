c1706402d4e8df2fe06e53d581193badacc2ae62
sadiqmeherremli <sadiqmeherremli.dev@gmail.com>
2024-05-10 03:10:31 +0400

    commit

---
+++ pronia_web/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pronia_web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
